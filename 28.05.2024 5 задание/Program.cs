7f1f5c7843df6c7859aeab57e3eb1b3a47f9c8c4
sasha <sashaislyamov7@mail.ru>
2024-05-28 15:08:19 +0300

    Добавьте файлы проекта.

---
+++ 28.05.2024 5 задание/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] array1 = { 1, 2, 3, 3 };
+        int[] array2 = { 4, 5, 6, 4 };
+        string[] array3 = { "a", "aa", "bb", "cd" };
+        string[] array4 = { "dd", "ee", "r", "cc" };
+
+        int[] mergedArray = MergeArrays(array1, array2);
+        string[] mergedArray1 = MergeArrays(array3, array4);
+
+        Console.WriteLine("Слияние массивов чисел:");
+        PrintArray(mergedArray);
+
+        Console.WriteLine("Слияние массивов строк:");
+        PrintArray(mergedArray1);
+    }
+
+    static T[] MergeArrays<T>(T[] array1, T[] array2)
+    {
+        T[] mergedArray = new T[array1.Length + array2.Length];
+
+        Array.Copy(array1, mergedArray, array1.Length);
+        Array.Copy(array2, 0, mergedArray, array1.Length, array2.Length);
+
+        return mergedArray;
+    }
+    static void PrintArray<T>(T[] array)
+    {
+        foreach (var element in array)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}
